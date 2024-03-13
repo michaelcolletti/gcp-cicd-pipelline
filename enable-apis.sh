744945c607c6a1d30e9d2a0140c9a0cf56b7707f
Michael <49289876+michaelcolletti@users.noreply.github.com>
2024-03-13 18:23:23 -0400

    add source repos to enable-apis

--- enable-apis.sh
+++ enable-apis.sh
@@ -3,6 +3,7 @@
 gcloud services enable serviceusage.googleapis.com \
 container.googleapis.com \
 cloudbuild.googleapis.com \
+sourcerepo.googleapis.com \
 artifactregistry.googleapis.com \
 clouddeploy.googleapis.com \
 cloudresourcemanager.googleapis.com
