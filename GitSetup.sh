812e24a99163b1c6a12a607e81ba86a76eed94b2
Michael <49289876+michaelcolletti@users.noreply.github.com>
2024-03-13 18:26:53 -0400

    Fix Github setup script

--- GitSetup.sh
+++ GitSetup.sh
@@ -5,24 +5,24 @@
 printf "First init your repo and correct set your global values for your Git user \n"
 
 
-```
+
 git init
 git config --global user.email "you@example.com"
 git config --global user.name "Student"
-```
 
-- Add the work in the present working directory (.) to staging (via a commit and helpful comment): 
-```
+
+printf "Add the work in the present working directory (.) to staging (via a commit and helpful comment): \n"
+
 git add .
 git commit -m "Initial commit and helpful specific comment"
-```
 
-- Create a new helloworld Git repository in your Cloud repository:
-```
+
+printf "Create a new helloworld Git repository in your Cloud repository: \n"
+
 gcloud source repos create helloworld
-```
-- Create a remote *to-google* for your repo 
-```
+
+printf "#- Create a remote *to-google* for your repo \n"
+
 git remote add to-google https://source.developers.google.com/p/$PROJECT_ID/r/helloworld
-```
+
 
